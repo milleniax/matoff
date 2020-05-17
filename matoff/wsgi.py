d06b63d488070dd4c0ab77028dbede594a7e0945
milleniax <marsel.abdullin.00@mail.ru>
2020-05-17 15:46:49 +0300

    Install gunicorn

--- matoff/wsgi.py
+++ matoff/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+from whitenoise.django import DjangoWhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matoff.settings')
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
