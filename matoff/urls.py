e4c48f02fc201ee7bc5f316d87472bb40103c1e4
milleniax <marsel.abdullin.00@mail.ru>
2020-05-17 15:19:34 +0300

    change account urls

--- matoff/urls.py
+++ matoff/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('', include('account.urls')),
 ]
 
 
