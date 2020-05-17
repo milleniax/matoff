6eedc540db88bda398883ab4a1f5abb3176ccdff
milleniax <marsel.abdullin.00@mail.ru>
2020-05-17 17:02:26 +0300

    Heroku storage

--- matoff/settings.py
+++ matoff/settings.py
@@ -144,3 +144,5 @@ EMAIL_USE_TLS = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
